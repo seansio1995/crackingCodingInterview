094646cf60ab774bce360c85fabded3cb53f5fac
seansio1995 <seansio@163.com>
2018-05-29 15:58:22 -0700

    stable version partition linked list

---
+++ src/linkedListPartition2.java
@@ -0,0 +1,34 @@
+public class linkedListPartition2 {
+    class LinkedListNode{
+        LinkedListNode next=null;
+        int data;
+    }
+    // In this version, we only grow the list based on the original linked list, we don't create
+    // new list anymore
+
+    LinkedListNode partition(LinkedListNode node, int x) {
+        LinkedListNode head = node;
+        LinkedListNode tail = node;
+
+        while(node!=null){
+            LinkedListNode next=node.next;
+
+            if(node.data < x){
+                // insert before the head
+                node.next=head;
+                head=node;
+
+            }
+            else{
+                //insert at the tail
+                tail.next=node;
+                tail=node;
+            }
+            node=next;
+        }
+
+        tail.next=null;
+
+        return head;
+    }
+}
