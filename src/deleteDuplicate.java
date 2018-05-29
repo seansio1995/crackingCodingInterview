8dd3d7ecdcb3350f86d52b8b829ffafab53973da
seansio1995 <seansio@163.com>
2018-05-29 14:29:57 -0700

    delete duplicate from unsorted linked list using hashmap

---
+++ src/deleteDuplicate.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+public class deleteDuplicate {
+    // delete duplicate from the unsorted linked list
+
+    class LinkedListNode{
+        LinkedListNode next=null;
+        int data;
+    }
+    void deleteDuplicate(LinkedListNode n){
+        HashSet<Integer> set=new HashSet<Integer>();
+        LinkedListNode previous=null;
+        while(n!=null){
+            if (set.contains(n.data)){
+                previous.next=n.next;
+            }
+            else{
+                set.add(n.data);
+                previous=n;
+            }
+            n=n.next;
+        }
+    }
+}
