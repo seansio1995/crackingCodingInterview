3b23f8f9f99b8aa936bc69264f3fcafb0e57d534
seansio1995 <seansio@163.com>
2018-05-29 16:29:07 -0700

    sum two lists

---
+++ src/sumList.java
@@ -0,0 +1,41 @@
+public class sumList {
+    class LinkedListNode{
+        LinkedListNode next=null;
+        int data;
+    }
+
+    // addition in the backward order
+
+    // 7 -> 1 -> 6
+    // 5 -> 9 -> 2
+
+    // add from the beginning with the carry
+
+    LinkedListNode addList(LinkedListNode l1, LinkedListNode l2, int carry){
+        if(l1==null || l2==null || carry==0)
+        {
+            return null;
+
+        }
+
+        LinkedListNode result=new LinkedListNode();
+        int value=carry;
+        if(l1!=null){
+            value+=l1.data;
+        }
+
+        if(l2!=null){
+            value+=l2.data;
+        }
+
+        result.data=value%10;
+
+
+        //recursion ????
+        if(l1!=null || l2!=null){
+            LinkedListNode more=addList(l1==null? null:l1.next, l2==null? null:l2.next,value>=10 ? 1:0 );
+            result.setNext(more);
+        }
+        return result;
+    }
+}
