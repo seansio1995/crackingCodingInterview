ef98a2b57109123e8c283e873bcc64a61ce11e83
seansio1995 <seansio@163.com>
2018-05-29 15:36:25 -0700

    delete the node in the middle

---
+++ src/deleteMiddleNode.java
@@ -0,0 +1,24 @@
+public class deleteMiddleNode {
+    // Before: a->b->c->d->e->f
+    // After: a ->b->d- >e- >f
+
+//    In this p roblem, you are not given access to the head of the linked list. You only have access to that node.
+//    The solution is simply to copy the data from the next node over to the current node, and then to delete the
+//    next node.
+
+    class LinkedListNode{
+        LinkedListNode next=null;
+        int data;
+    }
+    boolean deleteNode(LinkedListNode node){
+        if(node==null || node.next==null){
+            return false;
+        }
+
+        LinkedListNode next=node.next;
+        node.data=next.data;
+        node.next=next.next;
+
+        return true;
+    }
+}
